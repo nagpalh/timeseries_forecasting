91fbee91c1bdd9436951497e17a43e01631efd28
nagpalh <nagpalh@tcd.ie>
2025-11-17 13:26:59 +0100

    Add: Web-app deployment capability

--- main.py
+++ main.py
@@ -753,4 +753,5 @@ def forecast(dataset_id: int) -> str:
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=os.environ.get("FLASK_DEBUG") == "1")
